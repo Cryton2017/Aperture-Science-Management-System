89d989cd497984a036385d1c511bf993be73c41d
Christopher Littlewood <clittlewood50@gmail.com>
2021-09-04 09:08:49 +1000

    Ignoring Config File
    Ignoring the config file.

--- API/config.php
+++ API/config.php
@@ -5,6 +5,7 @@
     $port = '3306';
     $dbname = 'ApertureScienceMS';
 
+    //Connection string details:
     $connect_tring = "mysql:host={$host};port={$port};dbname={$dbname}";
     $username = 'admin';
     $password = 'hJUkRcTVo9mkRNHf9dSk';
